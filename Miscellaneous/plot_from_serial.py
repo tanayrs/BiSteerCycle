2cf6f188a7a81b06c2ab81d6f8bf012c252adc3d
tanayrs <114277985+tanayrs@users.noreply.github.com>
2024-06-11 15:28:09 +0530

    Update plot from serial

--- Miscellaneous/plot_from_serial.py
+++ Miscellaneous/plot_from_serial.py
@@ -4,7 +4,7 @@ import csv
 from matplotlib import pyplot as plt
 import pandas as pd
 
-PATH = 'SpeedData.csv'
+PATH = 'SensorData.csv'
 COM = '/dev/cu.usbmodem160464801'
 BAUD = 9600
 
@@ -53,16 +53,6 @@ def plot_from_csv():
     plt.xlabel('Time')
     plt.ylabel('Angle')
     plt.legend(['Front Angle','Rear Angle'])
-
-    # Setting y-limits to +- 6*std #
-    front_angle_mean = df['Front Angle'].mean()
-    front_angle_std = df['Rear Angle'].std()
-    rear_angle_mean = df['Front Angle'].mean()
-    rear_angle_std = df['Rear Angle'].std()
-    # plt.ylim(
-    #         [max(abs(front_angle_mean),abs(rear_angle_mean)) - (6*max(abs(front_angle_std),abs(rear_angle_std))), 
-    #          max(abs(front_angle_mean),abs(rear_angle_mean)) - (6*max(abs(front_angle_std),abs(rear_angle_std)))]
-    # )
 
     # Plotting Graph #
     plt.show()
