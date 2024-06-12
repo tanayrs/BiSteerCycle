b380dbd03400800142a94914fea7811f8a62ec53
tanayrs <114277985+tanayrs@users.noreply.github.com>
2024-06-12 09:26:12 +0530

    modifying deadband plotting

--- Python/dead band/plot_from_serial.py
+++ Python/dead band/plot_from_serial.py
@@ -1,13 +1,22 @@
+'''
+Experiment: Dead Band Testing
+By: Jia Bhargava, Tanay Srinivasa
+Last Modified: 12 June 2024
+
+Run bi_steer_cycle arduino code with set_dead_band_speed()
+'''
+
 import serial
 import time
 import csv
 from matplotlib import pyplot as plt
 import pandas as pd
 
-PATH = 'RearSpeedData.csv'
+PATH = 'RearDeadBandData.csv'
 COM = '/dev/cu.usbmodem160464801'
 BAUD = 9600
 
+# Reads Rear Wheel Data from Serial Channel and Stores to CSV #
 def read_from_serial():
     # Adding Header Row with Columns of CSV #
     with open(PATH, mode='w') as sensor_file:
@@ -17,7 +26,7 @@ def read_from_serial():
                 quotechar='"', 
                 quoting=csv.QUOTE_MINIMAL
                 )
-        sensor_writer.writerow(["Time", "Rear Speed"])
+        sensor_writer.writerow(["Time", "Rear Wheel Input", "Rear Wheel Speed"])
     
     # Creating Serial Object for COM Port and Selected Baud Rate #
     x = serial.Serial(COM, BAUD, timeout=0.1)
@@ -47,11 +56,12 @@ def plot_from_csv():
     print(df.describe())
 
     # Plotting Angle vs Time #
-    plt.plot(df['Time'],df['Rear Speed'])
+    plt.plot(df['Time'],df['Rear Wheel Input'])
+    plt.plot(df['Time'],df['Rear Wheel Speed'])
 
     plt.xlabel('Time')
     plt.ylabel('Speed')
-    plt.legend(['Rear Speed'])
+    plt.legend(['Rear Wheel Input', 'Rear Wheel Speed'])
 
     # Plotting Graph #
     plt.show()
