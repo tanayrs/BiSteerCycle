7fba11b0e25e45c6235b0a9f6dfa3d766b304a57
tanayrs <114277985+tanayrs@users.noreply.github.com>
2024-06-22 08:24:54 +0530

    front and rear compensation plotting

--- Python/dead band slope/deadband_plotting.py
+++ Python/dead band slope/deadband_plotting.py
@@ -32,10 +32,10 @@ REAR_DEADBAND_END_4 = 62937
 REAR_DEADBAND_INPUT_START = -160
 REAR_DEADBAND_INPUT_END = 170
 
-FRONT_COMP_DEADBAND_END_1 = 12840
-FRONT_COMP_DEADBAND_END_2 = 34054
-FRONT_COMP_DEADBAND_END_3 = 55066
-FRONT_COMP_DEADBAND_END_4 = 76309
+FRONT_COMP_DEADBAND_END_1 = 10526
+FRONT_COMP_DEADBAND_END_2 = 28583
+FRONT_COMP_DEADBAND_END_3 = 45684
+FRONT_COMP_DEADBAND_END_4 = 63689
 
 REAR_COMP_DEADBAND_END_1 = 10520
 REAR_COMP_DEADBAND_END_2 = 28132
@@ -152,6 +152,7 @@ def plot_front_compensated():
     # Reading CSV into pandas DataFrame #
     df = pd.read_csv(FRONT_COMP_PATH)
     df['Relative Time'] = df['Time']-df['Time'].iloc[0]
+    print(df['Time'].iloc[0])
 
     # Plotting Angle vs Time #
     fig, axs = plt.subplots(2, 1)
@@ -160,34 +161,33 @@ def plot_front_compensated():
 
     # Plot on the first axis
     axs[0].plot(df['Relative Time'], df['Wheel Input'])
-    # axs[0].axvline(x=FRONT_COMP_DEADBAND_END_1, color='k', linestyle='--', linewidth=0.5)
-    # axs[0].axvline(x=FRONT_COMP_DEADBAND_END_2, color='k', linestyle='--', linewidth=0.5)
-    # axs[0].axvline(x=FRONT_COMP_DEADBAND_END_3, color='k', linestyle='--', linewidth=0.5)
-    # axs[0].axvline(x=FRONT_COMP_DEADBAND_END_4, color='k', linestyle='--', linewidth=0.5)
-    # axs[0].axhline(y=FRONT_DEADBAND_INPUT_START, color='k', linestyle='--', linewidth=0.5)
-    # axs[0].axhline(y=FRONT_DEADBAND_INPUT_END, color='k', linestyle='--', linewidth=0.5)
-    # axs[0].plot(FRONT_COMP_DEADBAND_END_1,FRONT_DEADBAND_INPUT_END,'o')
-    # axs[0].plot(FRONT_COMP_DEADBAND_END_2,FRONT_DEADBAND_INPUT_START,'o')
-    # axs[0].plot(FRONT_COMP_DEADBAND_END_3,FRONT_DEADBAND_INPUT_END,'o')
-    # axs[0].plot(FRONT_COMP_DEADBAND_END_4,FRONT_DEADBAND_INPUT_START,'o')
+    axs[0].axvline(x=FRONT_COMP_DEADBAND_END_1, color='k', linestyle='--', linewidth=0.5)
+    axs[0].axvline(x=FRONT_COMP_DEADBAND_END_2, color='k', linestyle='--', linewidth=0.5)
+    axs[0].axvline(x=FRONT_COMP_DEADBAND_END_3, color='k', linestyle='--', linewidth=0.5)
+    axs[0].axvline(x=FRONT_COMP_DEADBAND_END_4, color='k', linestyle='--', linewidth=0.5)
+    
+    axs[0].axhline(y=FRONT_DEADBAND_INPUT_START, color='k', linestyle='--', linewidth=0.5)
+    axs[0].axhline(y=FRONT_DEADBAND_INPUT_END, color='k', linestyle='--', linewidth=0.5)
+    
+    axs[0].plot(FRONT_COMP_DEADBAND_END_1,FRONT_DEADBAND_INPUT_START,'o')
+    axs[0].plot(FRONT_COMP_DEADBAND_END_2,FRONT_DEADBAND_INPUT_END,'o')
+    axs[0].plot(FRONT_COMP_DEADBAND_END_3,FRONT_DEADBAND_INPUT_START,'o')
+    axs[0].plot(FRONT_COMP_DEADBAND_END_4,FRONT_DEADBAND_INPUT_END,'o')
+    
     axs[0].set_ylabel('Commanded Input (PWM Value)',fontsize=14)
-    # axs[0].set_yticks([-50,-40,-30,-20,-11,-5,5,11,20,30,40,50])
+    axs[0].set_yticks([-800,-600,-400,-170,0,215,400,600,800])
     axs[0].set_xticks([])
     # axs[0].set_title('Commanded Input vs Time')
 
     # Plot on the second axis
     axs[1].plot(df['Relative Time'], df['Wheel Speed'])
-    # axs[1].axvline(x=FRONT_COMP_DEADBAND_END_1, color='k', linestyle='--', linewidth=0.5)
-    # axs[1].axvline(x=FRONT_COMP_DEADBAND_END_2, color='k', linestyle='--', linewidth=0.5)
-    # axs[1].axvline(x=FRONT_COMP_DEADBAND_END_3, color='k', linestyle='--', linewidth=0.5)
-    # axs[1].axvline(x=FRONT_COMP_DEADBAND_END_4, color='k', linestyle='--', linewidth=0.5)
-    # axs[1].plot(FRONT_COMP_DEADBAND_END_1,0,'o')
-    # axs[1].plot(FRONT_COMP_DEADBAND_END_2,0,'o')
-    # axs[1].plot(FRONT_COMP_DEADBAND_END_3,0,'o')
-    # axs[1].plot(FRONT_COMP_DEADBAND_END_4,0,'o')
+    axs[1].axvline(x=FRONT_COMP_DEADBAND_END_1, color='k', linestyle='--', linewidth=0.5)
+    axs[1].axvline(x=FRONT_COMP_DEADBAND_END_2, color='k', linestyle='--', linewidth=0.5)
+    axs[1].axvline(x=FRONT_COMP_DEADBAND_END_3, color='k', linestyle='--', linewidth=0.5)
+    axs[1].axvline(x=FRONT_COMP_DEADBAND_END_4, color='k', linestyle='--', linewidth=0.5)
+    axs[1].axhline(y=0, color='k', linestyle='--', linewidth=0.5)
     axs[1].set_xlabel('Time (ms)',fontsize=14)
     axs[1].set_ylabel('Response (Degrees per Second)',fontsize=14)
-    # axs[1].set_title('Response vs Time')
 
     # Show the plot
     plt.tight_layout()
@@ -254,10 +254,10 @@ def plot_rear_compensated():
     plt.xlabel('Commanded Value (PWM Input)')
     plt.ylabel('Response (Degrees Per Second)')
     plt.title('Commanded Value vs Response')
-    # plt.show()
+    plt.show()
 
 if __name__ == '__main__':
     # plot_front()
     # plot_rear()
-    plot_front_compensated()
-    # plot_rear_compensated()
+    # plot_front_compensated()
+    plot_rear_compensated()
