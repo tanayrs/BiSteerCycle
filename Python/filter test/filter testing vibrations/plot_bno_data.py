3d44166dbdaaa72977e7adf6f527bb691ea46a9f
bhargavajia <jia.msvv@gmail.com>
2024-07-13 17:05:08 +0530

    restructuring filter testing Arduino code

--- Python/filter test/filter testing vibrations/plot_bno_data.py
+++ Python/filter test/filter testing vibrations/plot_bno_data.py
@@ -19,7 +19,7 @@ def read_from_serial():
                 quotechar='"', 
                 quoting=csv.QUOTE_MINIMAL
                 )
-        sensor_writer.writerow(["Time", "Acceleration", "Kalman"])
+        sensor_writer.writerow(["Time", "Acceleration", "SFA", "Kalman"])
     
     # Creating Serial Object for COM Port and Selected Baud Rate #
     x = serial.Serial(COM, BAUD, timeout=0.1)
@@ -38,7 +38,7 @@ def read_from_serial():
                         quotechar='"', 
                         quoting=csv.QUOTE_MINIMAL
                         )
-                sensor_writer.writerow([line[0],line[1],line[2]])
+                sensor_writer.writerow([line[0],line[1],line[2],line[3]])
 
 # Plotting Sensor Value Readings #
 def plot_from_csv(path=PATH):
@@ -50,16 +50,16 @@ def plot_from_csv(path=PATH):
 
     # Plotting Angle vs Time #
     df['Relative time'] = df['Time'] - df['Time'][0]
-    plt.plot(df['Relative time'],df['Acceleration'])
+    plt.plot(df['Relative time'],df['SFA'])
     plt.plot(df['Relative time'],df['Kalman'])
-    plt.legend(['Accelereometer angle', 'Kalman filter angle'],fontsize=14)
+    plt.legend(['SFA', 'Kalman filter'],fontsize=14)
     plt.xlabel('Time (ms)', fontsize=14)
     plt.ylabel('Angle (degrees)', fontsize=14)
-    plt.title('Response to vibrations', fontsize=16)
+    plt.title('Vibration Test of the Bosch Sensor Fusion Algorithm and Kalman Filter', fontsize=16)
     plt.show()
 
 
 
 if __name__ == '__main__':
-    # read_from_serial()
-    plot_from_csv(PATH)
+    read_from_serial()
+    # plot_from_csv(PATH)
